6c608d655fdf9dbdc22fa75979cb15a31695936a
Albatool23 <103149789+Albatool23@users.noreply.github.com>
2022-08-14 20:07:16 +0300

    complete

--- PredictDestructuring.js
+++ PredictDestructuring.js
@@ -43,18 +43,19 @@
 
 
 //5
-const lastTest = {
-    key: 'value',
-    secondKey: [1, 5, 1, 8, 3, 3]
-}
-const { key } = lastTest;
-const { secondKey } = lastTest;
-const [ ,willThisWork] = secondKey;
-//Predict the output
-console.log(key);//output value
-console.log(secondKey);//output [1, 5, 1, 8, 3, 3]
-console.log(secondKey[0]); //output 1
-console.log(willThisWork);//output 5
+// const lastTest = {
+//     key: 'value',
+//     secondKey: [1, 5, 1, 8, 3, 3]
+// }
+// const { key } = lastTest;
+// const { secondKey } = lastTest;
+// const [ ,willThisWork] = secondKey;
+// //Predict the output
+// console.log(key);//output value
+// console.log(secondKey);//output [1, 5, 1, 8, 3, 3]
+// console.log(secondKey[0]); //output 1
+// console.log(willThisWork);//output 5
+
 
 
 
